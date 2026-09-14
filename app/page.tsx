1fbd52ed5a03aee528c6c1c7d8f6c7e8bb56dcdb
Brandon Kim <brandonkim0802@gmail.com>
2026-09-14 01:53:37 -0400

    wallet ui fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { FeedSidebar } from "@/app/FeedSidebar";
 import { FeedSwitcher } from "@/app/FeedSwitcher";
+import { WalletDesk } from "@/app/WalletDesk";
 import { GLOBAL_POSTS } from "@/app/lib/mockPosts";
 
 export default function Page() {
@@ -8,7 +9,10 @@ export default function Page() {
       <div className="feed-main">
         <FeedSwitcher posts={GLOBAL_POSTS} />
       </div>
-      <FeedSidebar />
+      <aside className="feed-sidebar">
+        <WalletDesk showConnection={false} />
+        <FeedSidebar />
+      </aside>
     </main>
   );
 }
